5376ed99abe7ae041173c4549f8bc7a89640ffbd
Cristian311 <sarofincristian@gmail.com>
2022-05-10 21:33:12 +0300

    napi chat

--- roleplay.cs
+++ roleplay.cs
@@ -18,7 +18,7 @@ public class MinesTest : Script
     {
         var pos = NAPI.Entity.GetEntityPosition(sender);
         var playerDimension = NAPI.Entity.GetEntityDimension(sender);
-
+        NAPI.Chat.SendChatMessageToPlayer(sender, "Bomba nuclearaaaaa");
         var prop = NAPI.Object.CreateObject(NAPI.Util.GetHashKey("prop_bomb_01"), pos - new Vector3(0, 0, 1f), new Vector3(), 255, playerDimension);
         var shape = NAPI.ColShape.CreateSphereColShape(pos, 10);
         shape.Dimension = playerDimension;
